3070505b23354fe8923705c11baaad6503a00fb6
goran0201 <gplanincic@gmail.com>
2022-06-27 11:21:16 +0200

    izmena class umesto function

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React, { useState, useEffect } from "react";
 
-export default function App() {
+export default class App extends React.Component {
+
+    render() {
     return (
         <div>
         </div>
     );
+}
 }
 
