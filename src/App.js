a1886eec802d1a4e9c0a7c98999737177ba9be86
goran0201 <gplanincic@gmail.com>
2022-06-27 12:14:24 +0200

    app promenjen

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 import React from "react";
+import Drivers from "./components/Drivers";
+import DriverDetails from "./components/DriverDetails";
+import Teams from "./components/Teams";
+import TeamDetails from "./components/TeamDetails";
+import Races from "./components/Races";
+import RaceDetails from "./components/RaceDetails";
+
 
 export default class App extends React.Component {
 
+    
     render() {
     return (
         <div>
+            <Drivers />
+            <DriverDetails />
+            <Teams />
+            <TeamDetails />
+            <Races />
+            <RaceDetails />
         </div>
     );
 }
--- src/components/Drivers.js
+++ src/components/Drivers.js
@@ -2,7 +2,20 @@ import React from "react";
 
 
 export default class Drivers extends React.Component {
+    state = {
+        allDrivers: []
+    }
 
+    getDrivers = async () => {
+        const url = "'http://ergast.com/api/f1/2013";
+        const response = await fetch(url);
+        console.log("resp", response.headers.get('content-type'));
+        const drivers = await response.json();
+        console.log("drivers resp", drivers);
+        this.setState({
+            allDrivers: drivers        
+        });
+    };
     
       render() {
         return (
