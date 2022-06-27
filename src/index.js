16e8608bec41215b1fa975105c4d3bf0c2d379d3
goran0201 <gplanincic@gmail.com>
2022-06-27 10:53:03 +0200

    App i index fajl sredjen

--- src/index.js
+++ src/index.js
@@ -1,29 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App"; // export deafault nacin
-// import { App } from "./App"; // export bez default
-import "./css/main.css";
-import "./scss/styles.scss"
+import App from "./App";
 
-/* class App extends React.Component {
-    render() {
-        return (
-            <div>
-                <h2>Hello React, Webpack & Babel !!!</h2>
-                <h3>Hello GP</h3>
-            </div>
-        );
-    }
-} */
-
-//novija sintaksa
 var root = ReactDOM.createRoot(document.getElementById("app"));
 root.render(<App />);
-
-// starija sintaksa ne koristi se od v18
-// ReactDOM.render(<App />, document.getElementById("app"));
-
-// 1. nacin poz kompo  cesce se koristi
-{/* <App /> */ }
-// 2. nacin poz kompo 
-{/* <App> </App> */ }
