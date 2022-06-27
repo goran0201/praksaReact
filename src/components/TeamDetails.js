f45e306593b923bfc46d9ce5c141637d424cf16e
svetlanakaraba <svetlanakaraba@yahoo.com>
2022-06-27 15:14:49 +0200

    app new

--- src/components/TeamDetails.js
+++ src/components/TeamDetails.js
@@ -3,13 +3,28 @@ import React from "react";
 
 export default class TeamDetails extends React.Component {
 
-    
-      render() {
-        return (
-            <div>
-                TeamDetails
-          </div>
-        );
-      }
+  state = {
+    details: []
+  };
+
+  componentDidMount() {
+    this.getDetails();
+  };
+
+  getDetails = async () => {
+    const url = "http://ergast.com/api/f1/2013/constructors/01/constructorStandings.json";
+  }
+
+
+
+
+
+  render() {
+    return (
+      <div>
+        TeamDetails
+      </div>
+    );
+  }
 
 }
--- src/components/Teams.js
+++ src/components/Teams.js
@@ -1,27 +1,54 @@
 import React from "react";
+import * as $ from "jquery";
 import TeamDetails from "./TeamDetails";
 
 
 export default class Teams extends React.Component {
 
-    
-      render() {
-        return (
-            <div>
-              <h1>Constructors Campionship</h1>
+  state = {
+    allTeams: []
+  };
+
+  componentDidMount() {
+    this.getTeams();
+  };
+
+  getTeams = async () => {
+    const url = "http://ergast.com/api/f1/2013/constructorStandings.json";
+    const data = await fetch(url);
+    console.log("data", data);
+    const teams = await data.json();
+    console.log("teams", teams);
+    const allTeams = teams.MRData.StandingsTable.StandingsLists[0].ConstructorStandings;
+    console.log("allTeams", allTeams);
+
+    this.setState({
+      allTeams: allTeams
+    });
+  };
+
+
+  render() {
+    return (
+      <>
+        <h1>Constructors Campionship</h1>
+        <p>Constructors Championship Standings - 2013</p>
+        {this.state.allTeams.map( (team, i) => {
+          return (
+            <div key = {i}>
               <table className = "table">
                 <thead>
                   <tr>
-                    <th>Constructors Championship Standings - 2013</th>
-                    <th></th>
-                    <th></th>
-                    <th></th>
-                    <th></th>
+                    <th>{team.position}</th>
+                    <th>{team.Constructor.constructorId}</th>
+                    <th><a href={team.Constructor.url}>Details</a></th>
+                    <th>{team.points}</th>
                   </tr>
                 </thead>
-              </table>     
-                    
+             </table>
             </div>
-        );
-      }
-}
+          );
+        })}
+      </>
+    )}}
+      
