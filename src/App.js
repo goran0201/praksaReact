d5d83a3e8c8ebea48fde9f67aaf436e5ee376b24
pionirka <sandra.grba@gmail.com>
2022-06-27 14:52:47 +0200

    races

--- src/App.js
+++ src/App.js
@@ -1,26 +1,11 @@
 import React from "react";
-import Drivers from "./components/Drivers";
-import DriverDetails from "./components/DriverDetails";
-import Teams from "./components/Teams";
-import TeamDetails from "./components/TeamDetails";
-import Races from "./components/Races";
-import RaceDetails from "./components/RaceDetails";
 
 
-export default class App extends React.Component {
 
-    
-    render() {
+export default function App() {
     return (
         <div>
-            <Drivers />
-            <DriverDetails />
-            <Teams />
-            <TeamDetails />
-            <Races />
-            <RaceDetails />
         </div>
     );
-}
 }
 
