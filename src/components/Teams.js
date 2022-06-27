44fb023226b3b7e12a2b973edb07abef0e1ece6c
svetlanakaraba <svetlanakaraba@yahoo.com>
2022-06-27 12:25:29 +0200

    teams change

--- src/components/Teams.js
+++ src/components/Teams.js
@@ -1,4 +1,5 @@
 import React from "react";
+import TeamDetails from "./TeamDetails";
 
 
 export default class Teams extends React.Component {
@@ -7,9 +8,20 @@ export default class Teams extends React.Component {
       render() {
         return (
             <div>
-             Teams
-          </div>
+              <h1>Constructors Campionship</h1>
+              <table className = "table">
+                <thead>
+                  <tr>
+                    <th>Constructors Championship Standings - 2013</th>
+                    <th></th>
+                    <th></th>
+                    <th></th>
+                    <th></th>
+                  </tr>
+                </thead>
+              </table>     
+                    
+            </div>
         );
       }
-
 }
