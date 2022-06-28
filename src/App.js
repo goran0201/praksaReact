51833de6aab87f5919dfb6e0bc83f22bc1a8d192
pionirka <sandra.grba@gmail.com>
2022-06-28 14:12:10 +0200

    race

--- src/App.js
+++ src/App.js
@@ -31,7 +31,7 @@ export default class App extends React.Component {
                         <Route path = "/races" exact component = {Races} />
                         <Route path = "/driverDetails/:driverId" exact component = {DriverDetails} />
                         <Route path = "/teamDetails/:constructorId" exact component = {TeamDetails} />
-                        <Route path = "/raceDetails/:racesId" exact component = {RaceDetails} />
+                        <Route path = "/raceDetails/:raceId" exact component = {RaceDetails} />
                     </Switch>
                 </Router>
 
