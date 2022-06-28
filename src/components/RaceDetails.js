d15733bd1a578d47ec1168c32db4cceb8bc27fcd
pionirka <sandra.grba@gmail.com>
2022-06-28 10:50:15 +0200

    qualifiersDetails

--- src/components/RaceDetails.js
+++ src/components/RaceDetails.js
@@ -1,9 +1,30 @@
 import React from "react";
-
+state=[
+  qualifiersDetails = [],
+  // resultsDetalis = []
+]
 
 export default class RaceDetails extends React.Component {
 
-    
+    getqualifiersDetails =async() => {
+
+      const racesId = this.props.match.params.racesId;
+    const url = "http://ergast.com/api/f1/2013/${racesId}/qualifying.json";
+    console.log(racesId);
+
+    const response = await fetch(url);
+    console.log("responseDT", response);
+    const qualifiers = await response.json();
+    console.log("qualifiers", qualifiers);
+    const qualifiersDetails = drivers.MRData.StandingsTable.StandingsLists[0].DriverStandings;
+    console.log("qualifiersDetails", qualifiersDetails);
+    this.setState({
+      qualifiersDetails: qualifiers
+    });
+  };
+
+
+
       render() {
         return (
             <div>
