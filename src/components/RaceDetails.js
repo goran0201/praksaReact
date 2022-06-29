0715609a71367a82d1293a741e6ea8f99a3681e8
pionirka <sandra.grba@gmail.com>
2022-06-29 11:30:28 +0200

    raceResults

--- src/components/RaceDetails.js
+++ src/components/RaceDetails.js
@@ -5,28 +5,39 @@ import React from "react";
 export default class RaceDetails extends React.Component {
 
   state = {
-    qualifyngDetails: []
+    qualifyngDetails: [],
+    raceResults: []
 
   };
 
   componentDidMount() {
     this.getQualifiersDetails();
+
   }
 
   getQualifiersDetails = async () => {
 
     const raceId = this.props.match.params.raceId;
     const url = `http://ergast.com/api/f1/2013/${raceId}/qualifying.json`;
+    const urlResults = `http://ergast.com/api/f1/2013/${raceId}/results.json`;
     console.log("raceId", raceId);
 
     const response = await fetch(url);
+    const response2 = await fetch(urlResults);
+
     console.log("response", response);
+
     const qualifiers = await response.json();
+    const results = await response2.json();
     console.log("qualifiers", qualifiers);
+
     const qualifiersDetails = qualifiers.MRData.RaceTable.Races[0].QualifyingResults;
+    const raceResults = results.MRData.RaceTable.Races[0].Results;
     console.log("qualifiersDetails", qualifiersDetails);
+
     this.setState({
-      qualifyngDetails: qualifiersDetails
+      qualifyngDetails: qualifiersDetails,
+      raceResults: raceResults
     });
   };
 
@@ -55,12 +66,43 @@ export default class RaceDetails extends React.Component {
                   <td> {qualifying.Driver.familyName}</td>
                   <td> {qualifying.Constructor.name}</td>
                   <td> {qualifying.Q3}</td>
-                  </tr>
+                </tr>
               </tbody>
             );
           })}
         </table>
 
+        <div>
+
+        </div>
+        <table className="table-race-details">
+          <thead>
+            <tr>
+              <th>Race results</th>
+            </tr>
+            <tr>
+              <th>Pos</th>
+              <th>Driver</th>
+              <th>Team</th>
+              <th>Result</th>
+              <th>Points</th>
+            </tr>
+          </thead>
+         
+          {this.state.raceResults.map((result, index) => {
+            return (
+              <tbody key={index}>
+                <tr>
+                  <td>{result.position}</td>
+                  <td> {result.Driver.familyName}</td>
+                  <td> {result.Constructor.name}</td>
+                  <td> {result.Time?.time}</td>
+                  <td> {result.points}</td>
+                </tr>
+              </tbody>
+            );
+          })}
+        </table>
       </div>
     );
   }
--- webpack.config.js
+++ webpack.config.js
@@ -24,6 +24,17 @@ module.exports = {
                     "css-loader",
                     "sass-loader"
                 ]
+            },
+            {
+                test: /\.(jpe?g|gif|png|svg)$/i,
+                use: [
+                {
+                  loader: 'url-loader',
+                  options: {
+                    limit: 10000
+                  }
+                }
+              ]
             }
         ]
     },
