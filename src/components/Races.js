4c8e065d57c6e78aba68b05689867403cad0c191
pionirka <sandra.grba@gmail.com>
2022-06-30 09:53:11 +0200

    test

--- src/components/Races.js
+++ src/components/Races.js
@@ -1,5 +1,6 @@
 import React from "react";
 import RaceDetails from "./RaceDetails";
+import Flag from 'react-flagkit';
 import history from "./../history";
 
 export default class Races extends React.Component {
@@ -7,6 +8,7 @@ export default class Races extends React.Component {
   state = {
     races: [],
     seasons: {}
+  
   }
 
 
